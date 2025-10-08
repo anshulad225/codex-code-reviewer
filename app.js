aa8dced2c71ed60ed96ff11afe8e27642161b590
anshulad225 <anshulad225@gmail.com>
2025-10-08 16:39:16 +0530

    feat: add risky lines for Codex to review

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ console.log('Hello Codex');
 
 const password = 'hardcoded-secret';
 eval('console.log(123)');
+
+// BAD: insecure demo
+const password = 'hardcoded-secret-123';
+eval('console.log(123)');
